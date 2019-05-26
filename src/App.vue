7164a1b300fa4ea9608dda3e863e6c48ecc0c877
huyg <huygiang1393@gmail.com>
2019-05-26 01:28:13 -0700

    first commit, able to add skills to dynamic array of skills

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <Skills/>
+  </div>
+</template>
+
+<script>
+import Skills from './components/Skills.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Skills
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Montserrat:400,700');
+
+body {
+  background-color: #EEEEEE;
+  font-family: 'Montserrat', sans-serif;
+  display: grid;
+  grid-template-rows: auto;
+  justify-items: center;
+  align-items: center;
+}
+body, html {
+  margin: 0;
+  height: 100%;
+}
+
+#app {
+/*   font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px; */
+  width:50%
+}
+
+body{
+  background-color:#eeeeee;
+}
+</style>
